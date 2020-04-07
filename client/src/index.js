57aefbb81dbea058b318b9a1f4e47b02375e230d
github username <github email address>
2020-04-07 12:04:25 -0400

    adds redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import userReducer from './reducers/userReducer.js'
+
+const reducer = combineReducers({
+  user: userReducer
+})
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
